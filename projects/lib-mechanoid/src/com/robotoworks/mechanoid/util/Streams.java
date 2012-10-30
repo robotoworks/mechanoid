b980faa4e3dfaad6f85b4ccfabfbda02e63d0646
Ian Warwick <fluxtah@Ians-MacBook-Pro.local>
2012-10-30 08:38:52 +0000

    removed warnings

--- projects/lib-mechanoid/src/com/robotoworks/mechanoid/util/Streams.java
+++ projects/lib-mechanoid/src/com/robotoworks/mechanoid/util/Streams.java
@@ -2,10 +2,6 @@ package com.robotoworks.mechanoid.util;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.nio.charset.Charset;
 
 public class Streams {
 	public static String readAllText(InputStream in) throws IOException {
