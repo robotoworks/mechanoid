8d7472102bca58be82045665b30a0eb89826f138
fluxtan <fluxtah@hotmail.com>
2013-02-08 06:56:32 +0000

    added issue prefix constant

--- plugins/com.robotoworks.mechanoid/src/com/robotoworks/mechanoid/validation/MechanoidIssueCodes.java
+++ plugins/com.robotoworks.mechanoid/src/com/robotoworks/mechanoid/validation/MechanoidIssueCodes.java
@@ -2,6 +2,6 @@ package com.robotoworks.mechanoid.validation;
 
 public class MechanoidIssueCodes {
 	protected static final String ISSUE_CODE_PREFIX = "com.robotoworks.mechanoid.validation.IssueCodes.";
-	public static final String MISSING_MECHANOID_LIBS = "missing_mechanoid_libs";
+	public static final String MISSING_MECHANOID_LIBS = ISSUE_CODE_PREFIX + "missing_mechanoid_libs";
 
 }
