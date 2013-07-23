507de1d300a59c7d7df4670592cc7b3c8ed5ba69
fluxtah <fluxtah@hotmail.com>
2013-07-23 11:32:19 +0100

    Fixed issue on Eclipse EE not adding mechanoid libs correctly, fixes #182

--- plugins/com.robotoworks.mechanoid.ops.ui/src/com/robotoworks/mechanoid/ops/ui/quickfix/OpServiceModelQuickfixProvider.java
+++ plugins/com.robotoworks.mechanoid.ops.ui/src/com/robotoworks/mechanoid/ops/ui/quickfix/OpServiceModelQuickfixProvider.java
@@ -1,8 +1,15 @@
 
 package com.robotoworks.mechanoid.ops.ui.quickfix;
 
+import org.eclipse.xtext.common.types.access.jdt.IJavaProjectProvider;
+
+import com.google.inject.Inject;
+import com.robotoworks.mechanoid.ui.MechanoidLibsInstaller;
 import com.robotoworks.mechanoid.ui.quickfix.MechanoidQuickfixProvider;
 
 public class OpServiceModelQuickfixProvider extends MechanoidQuickfixProvider {
-
+    @Inject
+    public OpServiceModelQuickfixProvider(IJavaProjectProvider projectProvider, MechanoidLibsInstaller libsInstaller) {
+        super(projectProvider, libsInstaller);
+    }
 }
