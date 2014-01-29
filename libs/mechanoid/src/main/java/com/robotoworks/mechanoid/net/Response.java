13682064b461d5713d54c52762dd65a74dc5c11a
Ian Warwick <ian.warwick@just-eat.com>
2014-01-29 09:17:47 +0000

    added functionality to provide mocked responses

--- libs/mechanoid/src/main/java/com/robotoworks/mechanoid/net/Response.java
+++ libs/mechanoid/src/main/java/com/robotoworks/mechanoid/net/Response.java
@@ -1,44 +1,42 @@
-/*
- * Copyright 2013 Robotoworks Limited
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.robotoworks.mechanoid.net;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.util.List;
 import java.util.Map;
 
-import com.robotoworks.mechanoid.util.Streams;
-
-/**
- * <p>A Mechanoid Net response that captures the HTTP response code and headers of a HTTP response.</p>
- * <p>The response defers parsing from the stream until {@link #parse()} is called, this allows examination
- * of the response code before reading the stream in case of an unexpected response code.</p>
- * 
- * @param <T> A result representing that which will be parsed when {@link #parse()} is called.
- */
-public class Response<T> {
+public interface Response<T> {
+	
 	public static final int HTTP_INVALID = -1;
-    public static final int HTTP_ACCEPTED = 202;
+	/**
+	 * Standard response for successful HTTP requests. The actual response will depend on 
+	 * the request method used. In a GET request, the response will contain an entity 
+	 * corresponding to the requested resource. In a POST request the response 
+	 * will contain an entity describing or containing the result of the action
+	 */
+	public static final int HTTP_OK = 200;
+	/**
+	 * The request has been fulfilled and resulted in a new resource being created.
+	 */
+	public static final int HTTP_CREATED = 201;
+	
+	/**
+	 * The request has been accepted for processing, but the processing has 
+	 * not been completed. The request might or might not eventually be acted upon, 
+	 * as it might be disallowed when processing actually takes place
+	 */
+	public static final int HTTP_ACCEPTED = 202;
+	/**
+	 * The server successfully processed the request, but is returning information 
+	 * that may be from another source
+	 */
+	public static final int HTTP_NOT_AUTHORITATIVE = 203;
+	public static final int HTTP_NO_CONTENT = 204;
+	public static final int HTTP_PARTIAL = 206;
     public static final int HTTP_BAD_GATEWAY = 502;
     public static final int HTTP_BAD_METHOD = 405;
     public static final int HTTP_BAD_REQUEST = 400;
     public static final int HTTP_CLIENT_TIMEOUT = 408;
     public static final int HTTP_CONFLICT = 409;
-    public static final int HTTP_CREATED = 201;
     public static final int HTTP_ENTITY_TOO_LARGE = 413;
     public static final int HTTP_FORBIDDEN = 403;
     public static final int HTTP_GATEWAY_TIMEOUT = 504;
@@ -48,14 +46,10 @@ public class Response<T> {
     public static final int HTTP_MOVED_PERM = 301;
     public static final int HTTP_MOVED_TEMP = 302;
     public static final int HTTP_MULT_CHOICE = 300;
-    public static final int HTTP_NO_CONTENT = 204;
     public static final int HTTP_NOT_ACCEPTABLE = 406;
-    public static final int HTTP_NOT_AUTHORITATIVE = 203;
     public static final int HTTP_NOT_FOUND = 404;
     public static final int HTTP_NOT_IMPLEMENTED = 501;
     public static final int HTTP_NOT_MODIFIED = 304;
-    public static final int HTTP_OK = 200;
-    public static final int HTTP_PARTIAL = 206;
     public static final int HTTP_PAYMENT_REQUIRED = 402;
     public static final int HTTP_PRECON_FAILED = 412;
     public static final int HTTP_PROXY_AUTH = 407;
@@ -68,23 +62,11 @@ public class Response<T> {
     public static final int HTTP_UNAVAILABLE = 503;
     public static final int HTTP_VERSION = 505;
     
-	private T mContent;
-	private HttpURLConnection mConn;
-	private Parser<T> mParser;
-	private int mResponseCode;
-	private Map<String, List<String>> mHeaders;
-	
-	private byte[] mInputBytes;
-	
-	private boolean mParsed;
-
 	/**
 	 * @return The HTTP Response Code, i.e.:- 200
 	 */
-	public int getResponseCode() {
-		return mResponseCode;
-	}
-	
+	public abstract int getResponseCode();
+
 	/**
 	 * <p>Checks to see if the response code is HTTP_OK and if not, throws
 	 * a {@link UnexpectedHttpStatusException}.</p>
@@ -92,12 +74,9 @@ public class Response<T> {
 	 * <p>In some circumstances it is useful to call this to enforce a post condition
 	 * on the response code to ensure its HTTP_OK before continuing</p>
 	 */
-	public void checkResponseCodeOk() throws UnexpectedHttpStatusException {
-		if(mResponseCode != HTTP_OK) {
-			throw new UnexpectedHttpStatusException(mResponseCode, HTTP_OK);
-		}
-	}
-	
+	public abstract void checkResponseCodeOk()
+			throws UnexpectedHttpStatusException;
+
 	/**
 	 * <p>Checks to see if the response code is the given response code and if not, throws
 	 * a {@link UnexpectedHttpStatusException}.</p>
@@ -105,73 +84,21 @@ public class Response<T> {
 	 * <p>In some circumstances it is useful to call this to enforce a post condition
 	 * on the response code to ensure its of a certain code before continuing</p>
 	 */
-	public void checkResponseCode(int responseCode) throws UnexpectedHttpStatusException {
-		if(mResponseCode != responseCode) {
-			throw new UnexpectedHttpStatusException(mResponseCode, HTTP_OK);
-		}
-	}
+	public abstract void checkResponseCode(int responseCode)
+			throws UnexpectedHttpStatusException;
 
 	/**
 	 * @return A Map of header fields
 	 */
-	public Map<String, List<String>> getHeaders() {
-		return mHeaders;
-	}
-
-	public Response(HttpURLConnection conn, Parser<T> parser) throws ServiceException {
-		mConn = conn;
-		mParser = parser;
-		try {
-		mResponseCode = conn.getResponseCode();
-		mHeaders = conn.getHeaderFields();
-		} catch (Exception e) {
-			throw new ServiceException(e);
-		}
-	}
+	public abstract Map<String, List<String>> getHeaders();
 
 	/**
 	 * Parses the response stream into <T>
 	 * @return The parsed response <T>
 	 * @throws ServiceException
 	 */
-	public T parse() throws ServiceException {
-		if(mParsed) {
-			return mContent;
-		}
-		
-		try {
-			InputStream stream = null;
-			if(mInputBytes == null) {
-				stream = getInputStream();
-			} else {
-				if(mInputBytes.length > 0) {
-					stream = new ByteArrayInputStream(mInputBytes);
-				}
-			}
-			
-			if(stream != null) {
-				mContent = mParser.parse(stream);
-			}
-			
-		} catch (Exception e) {
-			throw new ServiceException(e);
-		}
-		
-		mParsed = true;
-		
-		return mContent;
-	}
+	public abstract T parse() throws ServiceException;
 
-	private InputStream getInputStream() throws IOException {
-		// error stream is available only if there was a connection with the 
-		// server, the server returned an error and also returned some usefull 
-		// data. Example being status 404 with page to search for content.
-		InputStream stream = mConn.getErrorStream();
-		if (stream != null)
-			return stream;
-		return mConn.getInputStream();
-	}
-	
 	/**
 	 * Reads the stream as text, the underlying stream will first be copied
 	 * into a byte buffer before being returned as a string. this will make sure
@@ -181,16 +108,6 @@ public class Response<T> {
 	 * @return
 	 * @throws IOException
 	 */
-	public String readAsText() throws IOException {
-		if(mInputBytes == null) {
-			InputStream stream = getInputStream();
-			if(stream == null) {
-				mInputBytes = new byte[]{};
-			} else {
-				mInputBytes = Streams.readAllBytes(getInputStream());
-			}
-		}
-		
-		return Streams.readAllText(new ByteArrayInputStream(mInputBytes));
-	}
+	public abstract String readAsText() throws IOException;
+
 }
