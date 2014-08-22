f2b8aebb7b11edf2fe96453e0ea6e30444393c45
Mikolaj Leszczynski <mikolaj.leszczynski@just-eat.com>
2014-08-22 10:37:33 +0100

    Created a basic gradle plugin for mechanoid

---
+++ plugins/com.robotoworks.mechanoid.gradle/plugin/src/main/groovy/com/robotoworks/mechanoid/MechanoidPluginExtension.groovy
@@ -0,0 +1,9 @@
+package com.robotoworks.mechanoid;
+
+class MechanoidPluginExtension {
+	String mechanoidStandaloneJarPath
+    String modelPath
+    String outputPath
+    String stubPath
+    boolean recurse = false
+}
